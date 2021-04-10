cc2db87ff0d696e41466f797673032ca28061089
Parin Kobboon <punparin@gmail.com>
2021-04-10 12:36:50 +0700

    Refactor client initializing

--- defy/exchanges/Binance.py
+++ defy/exchanges/Binance.py
@@ -1,16 +1,16 @@
 from binance.client import Client
 from tabulate import tabulate
 import os
-
 class Binance:
     def __init__(self, priceFinder):
         self.platformName = "Binance"
-
+        
         self.binanceApiKey = os.getenv("binance_api_key")
         self.binanceApiSecret = os.getenv("binance_api_secret")
         self.headers = [self.platformName, "Price", "Balance", "Balance ($)"]
 
         self.priceFinder = priceFinder
+        self.client = Client(self.binanceApiKey, self.binanceApiSecret)
 
     def isUsable(self):
         return self.binanceApiSecret is not None and self.binanceApiSecret is not None
@@ -20,8 +20,7 @@ class Binance:
             return []
 
         wallet = []
-        client = Client(self.binanceApiKey, self.binanceApiSecret)
-        tokens = client.get_account()["balances"]
+        tokens = self.client.get_account()["balances"]
 
         for token in tokens:
             symbol = token["asset"]
