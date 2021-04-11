b548ae24019214b7dd3366b8d80bb7f52c4b42bd
Parin Kobboon <punparin@gmail.com>
2021-04-11 12:27:37 +0700

    Use mock instead of calling API

--- tests/test_PriceFinder.py
+++ tests/test_PriceFinder.py
@@ -1,11 +1,24 @@
 import pytest
 from defy.PriceFinder import PriceFinder
+import json
+import responses
+from configparser import ConfigParser
 
 
 @pytest.fixture
+@responses.activate
 def myPriceFinder():
+    with open("tests/mocks/pricefinder_pancake_endpoint.json", "r") as mock_definition:
+        mockReponse = json.load(mock_definition)
+
+    config = ConfigParser()
+    config.read("./config.ini")
+    pancakeEndpoint = config["DEFAULT"]["pancake_endpoint"]
+
+    responses.add(responses.GET, pancakeEndpoint, json=mockReponse, status=200)
+
     return PriceFinder()
 
 
 def test_getTokenPrice(myPriceFinder):
-    assert myPriceFinder.getTokenPrice("BUSD") == 1
+    assert myPriceFinder.getTokenPrice("TEST") == 1
